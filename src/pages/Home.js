e03155530c0549011006a2c3129783b105e87cfb
TomLemelle <tom.lemelle@hotmail.fr>
2021-03-22 00:08:08 +0100

    home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Biography from '../components/Biography';
+import HeroBanner from '../components/HeroBanner';
+import Job from '../components/Job';
+import Navigation from '../components/Navigation';
+import Separator from '../components/Separator';
 
 const Home = () => {
     return (
-        <div className="main">
-            Home
-        </div>
+        <>
+            <Navigation />
+            <HeroBanner />
+            <Separator title="Présentation" description="Vidéo / Biographie / Développement web" />
+            <Biography />
+            <Job />
+        </>
     );
 };
 

58f8b5c010548076dc225f579e137d3dc1348b92
TomLemelle <tom.lemelle@hotmail.fr>
2021-03-24 13:48:32 +0100

    passions component

---
+++ src/components/Passions.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Passions = () => {
+    return (
+        <section className="passions">
+            <div className="passions-article article-1">
+                <div className="passions-caption">
+                    <h3 className="passions-title">Développement web</h3>
+                </div>
+            </div>
+            <div className="passions-article article-2">
+                <div className="passions-caption">
+                    <h3 className="passions-title">Production audiovisuelle</h3>
+                </div>
+            </div>
+            <div className="passions-article article-3">
+                <div className="passions-caption">
+                    <h3 className="passions-title">StreetWorkout</h3>
+                </div>
+            </div>
+            <div className="passions-article article-4">
+                <div className="passions-caption">
+                    <h3 className="passions-title">Gaming</h3>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Passions;

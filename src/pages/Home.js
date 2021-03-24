4f88b6ce0d1f51296fbf33071712861f52f7fc02
TomLemelle <tom.lemelle@hotmail.fr>
2021-03-24 13:52:33 +0100

    add new components to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,11 @@ import HeroBanner from '../components/HeroBanner';
 import Job from '../components/Job';
 import Internship from '../components/milestone/Internship';
 import Navigation from '../components/Navigation';
+import Passions from '../components/Passions';
 import Separator from '../components/Separator';
 import Skills from '../components/Skills';
 import Training from '../components/Training';
+import Videos from '../components/Videos';
 
 const Home = () => {
     return (
@@ -22,6 +24,11 @@ const Home = () => {
             <Training />
             <Separator title="Expériences" description="Stages" />
             <Internship />
+            <Separator title="Mes passions" description="Développeur web / Montage vidéo / Street Workout / Gaming" />
+            <Passions />
+            <Separator title="Mes vidéos" description="Première Pro" />
+            <Videos />
+            <Separator title="Contact" description="Convaincu ?" />
         </>
     );
 };
